cd4e9e386b241b1612e966617a89e47694bcc94c
Nikos Konstantakis <nikonst@otenet.gr>
2017-10-13 22:55:17 +0300

    Update withoutString.java

--- Core/strings/withoutString.java
+++ Core/strings/withoutString.java
@@ -1,6 +1,8 @@
 package strings;
 
 /**
+* Created by Nikos on 8/22/2017.
+*
  Given two strings, base and remove, return a version of the base string where all instances of the remove string
  have been removed (not case sensitive). You may assume that the remove string is length 1 or more.
  Remove only non-overlapping instances, so with "xxx" removing "xx" leaves "x".
